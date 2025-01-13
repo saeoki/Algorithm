aecfbb1dfc5d9edf4219945efe9d836eb797e75d
saeoki <130990838+saeoki@users.noreply.github.com>
2025-01-13 22:30:20 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -2,3 +2,11 @@ def solution(s):
     s = list(s)
     s.sort(reverse=1)
     return ''.join(s)
+
+"""
+이렇게 list로 변환 후 sort해도 되지만 sorted 메소드를 사용하면 문자열에도 정렬기능을 사용할 수 있다.
+문자열을 list type으로 변환한 후 정렬을 하는 것이기에
+s1 = sorted(s, reverse=1)
+return ''.join(s1)
+으로 전개했어도 가능했을 것
+"""
