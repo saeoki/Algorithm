67f7614e0cfb2a8c0be6d4ff083e342505a6c677
saeoki <130990838+saeoki@users.noreply.github.com>
2025-01-13 23:48:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 105.57 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -2,6 +2,6 @@ def solution(arr):
     stack = []
     for i in arr :
         if stack and stack[-1] == i : # stack이 비어있는 경우에 인덱싱을 하면 에러가 발생
-            stack.pop()
+            stack.pop() # 대신 continue로 그냥 append 자체를 생략 시켜버릴수도 있겠음
         stack.append(i)
     return stack
