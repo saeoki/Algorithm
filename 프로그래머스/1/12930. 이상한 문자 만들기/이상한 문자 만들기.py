714db36960fe7ed1d9409adae328f46a5d1f4bd5
saeoki <130990838+saeoki@users.noreply.github.com>
2025-01-14 23:59:06 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -8,7 +8,9 @@ def word_changer(word) :
     return result
 
 def solution(s):
-    word_list = s.split(' ') # split은 기본으로 공백을 기준으로 구분, 굳이 ' ' 안해도 됨
+    word_list = s.split(' ')
+    # split은 기본으로 공백을 기준으로 구분하지만 연속된 공백도 하나의 공백으로 간주해버리기 때문에
+    # 정확히 공백 하나로 구분하는 것을 원한다면 이처럼 지정해줘야함
     for i in range(len(word_list)) :
         word_list[i] = word_changer(word_list[i])
     return ' '.join(word_list)
