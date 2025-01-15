11b564cca92aa14c356ea1f318804ebe2d80611d
saeoki <130990838+saeoki@users.noreply.github.com>
2025-01-15 12:08:08 +0900

    [level 1] Title: 삼총사, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -7,3 +7,18 @@ def solution(number):
                 if number[i] + number[j] + number[k] == 0 :
                     answer += 1
     return answer
+
+"""
+`combinations`이란 모듈을 사용해서 풀이할 수도 있음
+combinations은 정해진 길이의 모든 조합을 튜플 리스트 형태로 반환
+예시 : result = combinations(data, 3)
+[(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)]
+
+def solution (number) :
+    from itertools import combinations
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
+"""
