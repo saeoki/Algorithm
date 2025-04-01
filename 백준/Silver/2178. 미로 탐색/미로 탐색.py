3bdd3e9bee0bc40c5c2543c92600bdd42f6c90dc
saeoki <130990838+saeoki@users.noreply.github.com>
2025-04-01 16:22:28 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,29 @@
 from collections import deque
 
-def bfs(x, y) :
+def bfs() :
   queue = deque()
-  queue.append((x, y))
+  queue.append([0,0])
   while queue :
-    x, y = queue.popleft()
+    node = queue.popleft()
+    x, y = node[0], node[1]
     for i in range(4) :
       nx = x + dx[i]
       ny = y + dy[i]
-      if nx < 0 or nx >=n or ny < 0 or ny >= m :
+      if nx < 0 or nx >= n or ny < 0 or ny >= m :
         continue
       if graph[nx][ny] == 0 :
         continue
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = graph[x][y] + 1
-        queue.append((nx, ny))
-  # 가장 오른쪽 최 하단 노드의 값을 리턴
-  return graph[n - 1][m - 1]
+      elif graph[nx][ny] == 1:
+        graph[nx][ny] += graph[x][y]
+        queue.append([nx, ny])
+  print(graph[n-1][m-1])
 
 n, m = map(int, input().split())
-
 graph = []
 for i in range(n) :
   graph.append(list(map(int, input())))
 
-# 인덱스 순서대로 상, 하, 좌, 우
+# 인덱스 순서대로 상,하,좌,우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-print(bfs(0,0))
+bfs()
