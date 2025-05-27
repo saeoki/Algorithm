f75f42635c6fbaf60ebf68cfef76dc9adbbd61d7
saeoki <130990838+saeoki@users.noreply.github.com>
2025-05-27 17:53:43 +0900

    [Silver IV] Title: 수 찾기, Time: 808 ms, Memory: 48592 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -18,3 +18,12 @@ for i in list_m :
             start = mid + 1
     else :
         print(0)
+"""
+이진탐색의 point는
+1. 탐색 범위가 매우 크다면 탐색 속도가 가장 빠른 이진탐색을 떠올린다.
+2. start와 end 값을 미리 지정해놓고, start <= end 일때까지 mid를 계속 (start+end) // 2로
+지정해주며 list[mid]와 찾고자 하는 값을 비교함에 따라 start와 end를 조정해준다
+3. 가장 먼저 list[mid] == 찾고자 하는 값 i 이면 return or print
+   list[mid] > i 이면 end = mid - 1
+   list[mid] < i 이면 start = mid + 1
+"""
