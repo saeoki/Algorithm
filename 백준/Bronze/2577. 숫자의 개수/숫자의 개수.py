ba771579f2aff2df8018dc661f83fa037f77b21c
saeoki <130990838+saeoki@users.noreply.github.com>
2025-08-31 17:02:01 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -3,16 +3,7 @@ b = int(input())
 c = int(input())
 n = a*b*c
 
-n_dict = {'0' : 0,
-        '1' : 0,
-        '2' : 0,
-        '3' : 0,
-        '4' : 0,
-        '5' : 0,
-        '6' : 0,
-        '7' : 0,
-        '8' : 0,
-        '9' : 0}
+n_dict = {str(i): 0 for i in range(10)}
 
 for i in str(n):
     n_dict[i] += 1
