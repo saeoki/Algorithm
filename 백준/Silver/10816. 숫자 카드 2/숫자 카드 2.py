0f18ba90b404fef7578f741aaac58dd75b9cfe97
saeoki <130990838+saeoki@users.noreply.github.com>
2025-09-07 22:43:21 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 728 ms, Memory: 119584 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,20 +1,17 @@
-"""
-입력 범위가 굉장히 크기 때문에 이진탐색류의 알고리즘을 적용해야함
-M 리스트를 하나씩 순회하며 N 리스트에 몇개나 존재하는지를 확인하면 되는데
-bisect_left와 bisect_right를 사용하여 범위 탐색을 하면 될 듯 하다
-"""
-from bisect import bisect_left, bisect_right
 import sys
-n = int(sys.stdin.readline())
-n_list = list(map(int, sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-m_list = list(map(int, sys.stdin.readline().split()))
-n_list.sort()
+input = sys.stdin.readline
 
-result_list = []
-for i in m_list :
-    result = str(bisect_right(n_list, i) - bisect_left(n_list, i))
-    result_list.append(result)
+n = int(input())
+cards = list(map(int, input().split()))
+m = int(input())
+targets = list(map(int, input().split()))
 
-result_list = ' '.join(result_list)
-print(result_list)
+count = {}
+
+# N리스트 빈도수 체킹
+for c in cards:
+    count[c] = count.get(c, 0) + 1
+
+# M요소 탐색
+for t in targets:
+    print(count.get(t, 0), end=" ")
