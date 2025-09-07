b3e1a70bcfd03f1323dba599cb68c8fdaaf0265b
saeoki <130990838+saeoki@users.noreply.github.com>
2025-09-07 23:55:17 +0900

    [Silver I] Title: 트리 순회, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,33 @@
+n = int(input())
+tree = {}
+
+for _ in range(n) :
+    root, left, right = input().split()
+    tree[root] = (left, right)
+
+def preorder(x) :
+    if x == '.':
+        return
+    print(x, end = '')
+    preorder(tree[x][0])
+    preorder(tree[x][1])
+
+def inorder(x) :
+    if x == '.' :
+        return
+    inorder(tree[x][0])
+    print(x, end='')
+    inorder(tree[x][1])
+
+def postorder(x) :
+    if x == '.' :
+        return
+    postorder(tree[x][0])
+    postorder(tree[x][1])
+    print(x, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
