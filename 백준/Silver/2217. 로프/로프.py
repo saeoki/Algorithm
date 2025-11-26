b2dc8b2582cafe478be229faaabe547978168c79
saeoki <sw000620@gmail.com>
2025-11-26 22:07:31 +0900

    [Silver IV] Title: 로프, Time: 3240 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,30 @@
+"""
+N개의 로프가 주어지고 이 로프들을 병렬 연결해서 들 수 있는 최대의 중량을 구하기
+로프를 많이 연결한다고 좋은 것은 아님. 값이 유난히 작은 변수가 있으면 다른 큰 값을 못이겨냄
+너무 작은 로프는 안쓰는게 나음
+- (최소값 * 로프 갯수)
+- 최대값
+비교해서 후자가 더 크면 최소값을 제거하고 최소값을 뒤로 한칸 땡김
+
+test case)
+5
+5
+10
+20
+45
+50
+"""
+n = int(input())
+n_list = []
+for _ in range(n) :
+    n_list.append(int(input()))
+
+n_list.sort()
+max = n_list[-1]
+for _ in range(n) :
+    parallel = n_list[0] * len(n_list)
+    if parallel > max :
+        max = parallel
+    n_list.pop(0)
+
+print(max)
